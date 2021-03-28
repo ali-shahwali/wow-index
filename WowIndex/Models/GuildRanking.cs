4a1adb5645f8177b42cb4afaa381b55641fed979
Ali Shahwali <ali_zinq@hotmail.com>
2021-03-28 20:33:58 +0200

    top 100 guilds pulled

--- WowIndex/Models/GuildRanking.cs
+++ WowIndex/Models/GuildRanking.cs
@@ -2,37 +2,55 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-
+using Newtonsoft.Json;
 namespace WowIndex.Models
 {
     public class GuildRanking
     {
-        //    {
-        //  "guild": {
-        //    "name": "Honestly",
-        //    "id": 41321785,
-        //    "realm": {
-        //      "name": "Frostmourne",
-        //      "id": 3725,
-        //      "slug": "frostmourne"
-        //    }
-        //  },
-        //  "faction": {
-        //    "type": "ALLIANCE"
-        //  },
-        //  "timestamp": 1538129132000,
-        //  "region": "us",
-        //  "rank": 2
-        //} 
+        [JsonProperty("guild")]
+        public Guild guild { get; set; }
 
-        public int Id { get; set; }
-        public string Honestly { get; set; }
-        public string RealmName { get; set; }
-        public int RealmId { get; set; }
-        public string RealmSlug { get; set; }
-        public string Faciton { get; set; }
-        public string timestamp { get; set; }
+        [JsonProperty("faction")]
+        public Faction faction { get; set; }
+
+        [JsonProperty("timestamp")]
+        public long timestamp { get; set; }
+
+        [JsonProperty("region")]
         public string region { get; set; }
-        public int rank { get; set; }
+
+        [JsonProperty("rank")]
+        public long rank { get; set; }
+    }
+
+    public class Guild
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("id")]
+        public long id { get; set; }
+
+        [JsonProperty("realm")]
+        public Realm realm { get; set; }
+        
+    }
+
+    public class Realm
+    {
+        [JsonProperty("name")]
+        public string name { get; set; }
+
+        [JsonProperty("id")]
+        public long id { get; set; }
+
+        [JsonProperty("slug")]
+        public string slug { get; set; }
+    }
+
+    public class Faction
+    {
+        [JsonProperty("type")]
+        public string type { get; set; }
     }
 }
---
+++ WowIndex/Models/LeaderboardEntries.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace WowIndex.Models
+{
+    public class LeaderboardEntries
+    {
+        [JsonProperty("_links")]
+        public Links _links { get; set; }
+
+        [JsonProperty("slug")]
+        public string slug { get; set; }
+
+        [JsonProperty("criteria_type")]
+        public string criteria_type { get; set; }
+
+        [JsonProperty("entries")]
+        public GuildRanking[] entries { get; set; }
+
+        [JsonProperty("journal_instance")]
+        public JournalInstance JournalInstance { get; set; }
+    }
+
+    public class JournalInstance
+    {
+        [JsonProperty("key")]
+        public Self Key { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+    }
+
+    public class Links
+    {
+        [JsonProperty("self")]
+        public Self self { get; set; }
+    }
+
+    public class Self
+    {
+        [JsonProperty("href")]
+        public string href { get; set; }
+    }
+}
