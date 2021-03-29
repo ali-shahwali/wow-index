fcaee8a438ef35a941e3d776bec353cbf7667047
Ali Shahwali <ali_zinq@hotmail.com>
2021-03-29 08:43:40 +0200

    minor bug fixes

---
+++ WowIndex/Data/Migrations/20210329063314_Model6.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WowIndex.Data.Migrations
+{
+    public partial class Model6 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
