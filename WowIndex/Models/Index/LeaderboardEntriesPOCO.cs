d72b3ae69016accec8da4962b97c1447e30eba13
Ali Shahwali <ali_zinq@hotmail.com>
2021-03-31 12:25:31 +0200

    Visual changes and optimizations

--- WowIndex/Models/Index/LeaderboardEntriesPOCO.cs
+++ WowIndex/Models/Index/LeaderboardEntriesPOCO.cs
@@ -5,6 +5,51 @@ using System.Threading.Tasks;
 using Newtonsoft.Json;
 namespace WowIndex.Models.Index
 {
+    public class LeaderboardEntriesPOCO
+    {
+
+        [JsonProperty("_links")]
+        public Links _links { get; set; }
+
+        [JsonProperty("slug")]
+        public string slug { get; set; }
+
+        [JsonProperty("criteria_type")]
+        public string criteria_type { get; set; }
+
+        [JsonProperty("entries")]
+        public GuildRanking[] entries { get; set; }
+
+        [JsonProperty("journal_instance")]
+        public JournalInstance JournalInstance { get; set; }
+    }
+
+    public class JournalInstance
+    {
+        [JsonProperty("key")]
+        public Self Key { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("id")]
+        public long Id { get; set; }
+    }
+
+    public class Links
+    {
+
+        [JsonProperty("self")]
+        public Self self { get; set; }
+    }
+
+    public class Self
+    {
+
+        [JsonProperty("href")]
+        public string href { get; set; }
+    }
+
     public class GuildRanking
     {
 
@@ -34,7 +79,7 @@ namespace WowIndex.Models.Index
 
         [JsonProperty("realm")]
         public Realm realm { get; set; }
-        
+
     }
 
     public class Realm
