919d0e397264aae1d6d60b2b3cc8f9c661d04489
DevKeone <keone.somers@outlook.com>
2021-04-04 02:47:57 +0100

    wow armory scraper v1.0

---
+++ WowIndex/Models/POCO/ShadowlandsRaidPOCO.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace WowIndex.Models.POCO.ShadowlandsRaidPOCO
+{
+
+    public class ShadowlandsRaidPOCO
+    {
+        public Achievement[] achievements { get; set; }
+        public string id { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Achievement
+    {
+        public bool accountWide { get; set; }
+        public string description { get; set; }
+        public Icon icon { get; set; }
+        public int id { get; set; }
+        public string name { get; set; }
+        public int point { get; set; }
+        public object[] progressSteps { get; set; }
+        public Reward reward { get; set; }
+        public Step[] steps { get; set; }
+        public DateTime time { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Icon
+    {
+        public string url { get; set; }
+    }
+
+    public class Reward
+    {
+        public string name { get; set; }
+        public string type { get; set; }
+    }
+
+    public class Step
+    {
+        public bool completed { get; set; }
+        public string description { get; set; }
+        public Icon1 icon { get; set; }
+        public bool isGold { get; set; }
+        public string url { get; set; }
+    }
+
+    public class Icon1
+    {
+        public string url { get; set; }
+    }
+}
---
+++ WowIndex/Models/RaidAchievement.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WowIndex.Models
+{
+    public class RaidAchievement
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int StepsTotal { get; set; }
+        public int StepsCompleted { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
