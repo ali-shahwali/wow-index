712d5cb4b9fec735530b7dd50341565572416c66
DevKeone <keone.somers@outlook.com>
2021-04-06 20:24:24 +0100

    pushy wushy

---
+++ WowIndex/Models/BossStats.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace WowIndex.Models
+{
+    public class BossStats
+    {
+        public string Name { get; set; }
+        public DateTime KillTime { get; set; }
+    }
+}
